7647b1b6e7ad6109f76de0e71ed3169734fb92ed
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-08-10 16:26:36 -0400

    First commit

---
+++ PythonPCEPCourseAllNotebooks/Tutorial 1- Setup-Basics.py
@@ -0,0 +1,41 @@
+# Python IDE Class from https://www.youtube.com/watch?v=56bPIGf4us0
+
+import pygame
+
+pygame.init()
+
+p = 7  # if you want to change the variable name however there are many instances of that variable,
+# right click-refactor-rename
+print(p)
+print(p)
+print(p)
+
+
+def main():
+    x = 0  # note that refactoring a variable name is only within the scope of the variable
+    print(x)
+    print(x)
+    print(x)
+
+# short cuts:
+#   find: ctr + F (command + F)
+#   view recent changes to code: alt + shift + C (option + shift + C)
+#   duplicate previous actions: ctr + D (command + D)
+    print(x)
+    print(x)
+    print(x)
+    print(x)
+# Surround with:  Ctr + alt + T (command + options + T)
+run = True
+try:
+    run
+except:
+    pass
+# TODO complete this video.
+# TODO BTW, this comment allows to create todo points that will be saved in the todo section
+#  located at the bottom left
+
+
+
+
+
